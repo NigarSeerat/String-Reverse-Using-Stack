264eaf0333cc29103acbd93c2b4929cc66381c9f
NigarSeerat <47634756+NigarSeerat@users.noreply.github.com>
2020-09-24 12:24:16 +0530

    Update String reverse.cpp

--- String reverse.cpp
+++ String reverse.cpp
@@ -4,15 +4,3 @@
       Aerospace Engineer
 	  24 Sep, 2020
 */
-
-#include<iostream>
-#include<string>
-using namespace std;
-int main (void ) {
-	string str;
-    getline(cin,str);    //Taking Input string
-//	int len = str.size(); //getting the size of input string
-	for(int i=str.size(); i>0; i--) { 
-	cout<<str[i];     //Printing the reversed string
-    }
-}
